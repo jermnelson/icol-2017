e2046db1f2e30f4241dc4221882963d5f0db0766
Jeremy Nelson <jermnelson@gmail.com>
2017-07-04 13:51:38 -0600

    update default

--- static/js/default.js
+++ static/js/default.js
@@ -2,4 +2,12 @@ function pullAnimation() {
     var pull = Snap("#pull-img");
     alert("Pull is " + pull); 
     var circ = pull.circle(20, 20, 20);
+   var car_animation = Snap.load("/static/img/illustrating-pull.svg", 
+        function (loadedSVG) {
+            pull.append(loadedSVG); 
+            var finished_car = pull.select("#finished-red-car");
+            alert("Finished car is " + finished_car);
+            finished_car.animate({ x: 10 }, 3000);
+    });
+
 }
