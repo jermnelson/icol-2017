a1a8a8ed2057c5d920dace6ca1cd87f7f6f5a080
Jeremy Nelson <jermnelson@gmail.com>
2017-07-05 14:22:37 -0600

    working on animation

--- pages/pull-vs-push-for-library-services.md
+++ pages/pull-vs-push-for-library-services.md
@@ -1,16 +1,17 @@
 title: Pull verses Push for Library Services
 previous: what-is-a-lean-library
 next: build-measure-learn-project-management
-animations: pullAnimation
 
 ## Push 
-
-
 With it's origins in early modern industrial companies, with the Ford Motor Company 
 being the exemplar of this type of organization, **Push** is about the ability of 
 large companies and organizations to accurately predict and mold demand of 
 customers, organize their operations around those predictions, and then sell those
 products based upon the organization's predictions and planning. 
+
+<button onclick="pushAnimation()">Show Push <i class="fa fa-film"></i></button>
+
+<svg id="animate-push" width=800 height=600></svg>
 
 
 ## Pull
@@ -20,6 +21,9 @@ started with Toyota Production System principles and ideas, but expanded by
 and services by "pulling" directly from the organizations customers and potential 
 customers. 
 
+<button onclick="pullAnimation()">Show Pull <i class="fa fa-film"></i></button>
 
-<div id="pull-img"></div>
+
+<svg id="animate-pull" width=800 height=600></svg>
+
 
--- static/js/default.js
+++ static/js/default.js
@@ -1,13 +1,25 @@
 function pullAnimation() {
-    var pull = Snap("#pull-img");
-    alert("Pull is " + pull); 
+    var pull = Snap("#animate-pull");
     var circ = pull.circle(20, 20, 20);
     var car_animation = Snap.load("/static/img/illustrating-pull.svg", 
         function (loadedSVG) {
             pull.append(loadedSVG); 
             var finished_car = pull.select("#finished-red-car");
-            alert("Finished car is " + finished_car);
-            finished_car.animate({ x: 10 }, 3000);
+            var car_frame = pull.select("#car-frame");
+            var tires = pull.select("#tires");
+            finished_car.animate({ transform: 's0,1' }, 1000);
+            car_frame.animate({ path: "m 469, 201"}, 2000);
+            tires.animate({ transform: "s0,1"}, 1000);
     });
+
+}
+
+function pushAnimation() {
+    var push = Snap("#animate-push");
+    var car_grp = push.g();
+    var car_frame = car_grp.image("/static/img/car-shell.png");
+    //car_grp.animate(500, 500);
+    var tires_grp = push.g();
+    var tires = tires_grp.image("/static/img/tires.png");
 
 }
